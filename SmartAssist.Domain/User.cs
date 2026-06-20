6f49d110ebdd9255a1d93676e31397751e3e51b1
sanjit kumar <sanjit.kumar822@outlook.com>
2026-06-20 08:15:53 +0530

    Added Enum for Role

--- SmartAssist.Domain/User.cs
+++ SmartAssist.Domain/User.cs
@@ -8,9 +8,16 @@ namespace SmartAssist.Domain
     {
         public string UserId { get; set; }
         public string Name { get; set; }
-        public int Role { get; set; }
+        public UserRole Role { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
 
+    }
+
+    public enum UserRole
+    {
+        END_USER = 1,
+        SUPPORT_ENGINEER = 2,
+        SUPERVISOR = 3
     }
 }
--- SmartAssist.Repository/UserRepository.cs
+++ SmartAssist.Repository/UserRepository.cs
@@ -14,8 +14,8 @@ namespace SmartAssist.Repository
             // Mock data or DB call
             return new List<User>
             {
-                new User { UserId = "U001", Name = "Amit Sharma", Role = 1, Email = "amit.sharma@smartassist.com" },
-                new User { UserId = "U002", Name = "Priya Verma", Role = 1, Email = "priya.verma@smartassist.com" }
+                new User { UserId = "U001", Name = "Amit Sharma", Role = UserRole.END_USER, Email = "amit.sharma@smartassist.com" },
+                new User { UserId = "U002", Name = "Priya Verma", Role = UserRole.END_USER, Email = "priya.verma@smartassist.com" }
             };
         }
     }
